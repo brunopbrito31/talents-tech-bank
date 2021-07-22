bffb29ae531dbdd9debd4a3a334352c05471bf14
Bruno Brito <brunopbrito31@hotmail.com>
2021-07-22 11:49:16 -0300

    correcao de bug no main

--- src/projetosistemavendas/Main.java
+++ src/projetosistemavendas/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        ServicoDeMenu.cadastrarNovoProduto();
+        ServicoDeMenu.cadastrarNovosProdutos();
 
 
     }
